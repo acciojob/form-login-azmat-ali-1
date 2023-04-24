65937035e12b0918a3015c08689e700b2240e495
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-25 03:24:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+const f = document.getElementById("myForm");
+  const firstName = form.elements["fname"].value;
+  const lastName = form.elements["lname"].value;
+  alert(firstName," ",lastName);
+	
 }
